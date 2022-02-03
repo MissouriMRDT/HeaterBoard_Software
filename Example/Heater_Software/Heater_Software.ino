edd5e6a75004c01d095c231a2d7e88060b408dfe
Grant Brinker <gsbfy2@umsystem.edu>
2022-02-02 18:41:50 -0600

    Finished Rovecomm integration

--- Example/Heater_Software/Heater_Software.ino
+++ Example/Heater_Software/Heater_Software.ino
@@ -3,44 +3,70 @@
 void setup()
 {
   // inputs to MOSFET circuits
-  pinMode(PIN_2, OUTPUT);
-  pinMode(PIN_6, OUTPUT);
-  pinMode(PIN_9, OUTPUT);
+  pinMode(TEMP_SENSE_PIN_1, OUTPUT);
+  pinMode(TEMP_SENSE_PIN_2, OUTPUT);
+  pinMode(TEMP_SENSE_PIN_3, OUTPUT);
   
   // data from temperature sensors
-  pinMode(PIN_A17, INPUT);
-  pinMode(PIN_A16, INPUT);
-  pinMode(PIN_A15, INPUT);
-  float temps[3] = {temp1Celsius, temp2Celsius, temp3Celsius};
+  pinMode(TEMP_SENSE_DATA_1, INPUT);
+  pinMode(TEMP_SENSE_DATA_2, INPUT);
+  pinMode(TEMP_SENSE_DATA_3, INPUT);
+
 
   Serial.begin(9600);
+
+  //Set up rovecomm with the correct IP and the TCP server
+  RoveComm.begin(RC_HEATERBOARD_FOURTHOCTET, &TCPServer);
+  delay(100);
+
+  Serial.println("Started: ");
   
-  RoveComm.begin(RC_HEATERBOARD_FOURTHOCTET, &TCPSERVER);
+  //update timekeeping
+  last_update_time = millis();
 }
 
 void loop()
 {
+  // temperature data from each sensor
+  float temp1 = analogRead(TEMP_SENSE_DATA_1);
+  float temp2 = analogRead(TEMP_SENSE_DATA_2);
+  float temp3 = analogRead(TEMP_SENSE_DATA_3);
+
+  float temp1Celsius = map(temp1, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
+  float temp2Celsius = map(temp2, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
+  float temp3Celsius = map(temp3, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
+
+  float temps[3] = {temp1Celsius, temp2Celsius, temp3Celsius};
+  
+  // monitoring temperature values and toggling individual heaters
   for (int i == 0; i < 3; i++)
   {
       switch(temps[i])
     {
       case >= 105 :
-        digitalWrite(TEMP_SENSE_PIN_1, LOW);
+        digitalWrite(TEMP_PINS[i], LOW);
         break;
       case <= 95  :
-        digitalWrite(TEMP_SENSE_PIN_1, HIGH);
+        digitalWrite(TEMP_PINS[i], HIGH);
+        break;
+      case >= 115 :
+        Rovecomm.write(RC_HEATERBOARD_OVERHEAT_DATA_ID); // sends warning to basestation
         break;
     }
   }
-  ////// Still Needed: //////
-  // switch case for RoveComm needed
-  // communication with basestation
-  // overheat warning for basestation
-  
-  
-  if(millis()-last_update_time >= ROVECOMM_UPDATE_RATE)
-  {
 
+  
+  switch (packet.data_id)
+  {
+    case RC_HEATERBOARD_HEATERTOGGLE_DATA_ID  :
+      Serial.println("The heater has been toggled")
+      break;
   }
   
+  // sends data to basestation
+  if(millis()-last_update_time >= ROVECOMM_UPDATE_RATE)
+  {
+    Rovecomm.write(RC_HEATERBOARD_HEATERENABLED_DATA_ID, RC_HEATERBOARD_THERMO_VALUES_DATA_ID, RC_HEATERBOARD_THERMO_VALUES_DATA_COUNT, temps);
+    last_update_time = millis();
+  }
 }
--- src/Heater_Software.h
+++ src/Heater_Software.h
@@ -1,12 +1,5 @@
 #include "RoveComm.h"
 
-// temperature data from each sensor
-float temp1 = analogRead(PIN_A17);
-float temp2 = analogRead(PIN_A16);
-float temp3 = analogRead(PIN_A15);
-float temp1Celsius = map(temp1, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
-float temp2Celsius = map(temp2, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
-float temp3Celsius = map(temp3, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
 
 // conversions from ADC values to Celsius
 float TEMP_ADC_MIN = 0;
@@ -19,6 +12,8 @@ float TEMP_MAX = 0;
 #define TEMP_SENSE_PIN_2    6
 #define TEMP_SENSE_PIN_3    9
 
+int TEMP_PINS[3] = {TEMP_SENSE_PIN_1, TEMP_SENSE_PIN_2, TEMP_SENSE_PIN_3};
+
 // data from sensors
 #define TEMP_SENSE_DATA_1   A17
 #define TEMP_SENSE_DATA_2   A16
@@ -30,12 +25,11 @@ float TEMP_MAX = 0;
 RoveCommEthernet RoveComm;
 rovecomm_packet packet;
 
-int16_t motor_speed[6] = {-500, 200, 740, -720, 10, -182};
 
 //timekeeping variables
 uint32_t last_update_time;
 
 //declare the Ethernet Server in the top level sketch with the requisite port ID any time you want to use RoveComm
-EthernetServer TCPServer(RC_ROVECOMM_DRIVEBOARD_PORT);
+EthernetServer TCPServer(RC_ROVECOMM_HEATERBOARD_PORT);
 
 last_update_time = millis();
